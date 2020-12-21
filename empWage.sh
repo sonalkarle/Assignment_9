8ecface649d3b2394cddb6f7438ad9d91f24f5cc
sonal <sonalkarle01@gmail.com>
2020-12-21 18:58:28 +0530

    Calculating employee Wage

--- empWage.sh
+++ empWage.sh
@@ -1,2 +1,16 @@
-echo "Welcome to empwage code"
-
+#!/bin/bash -x
+#calculate the dailywage of Employee
+#Take one constant
+	ispresent=1;
+#Generate input by using random command 
+	randomCheck=$((RANDOM%2))
+#Use if condition to compare input with constant to calculate dailywage for present employee
+	if [[ $ispresent -eq $randomCheck ]]
+	then
+		empRatePerHr=20
+		empHrs=8
+		salary=$(($empHrs*$empRatePerHr))
+	else
+		salary=0
+	fi
+	
