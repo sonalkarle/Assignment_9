a996cbd2580e3878c136b8f12622fb018e954247
sonal <sonalkarle01@gmail.com>
2020-12-21 19:09:41 +0530

    Adding Part time Employee wage

--- empWage.sh
+++ empWage.sh
@@ -1,2 +1,23 @@
-echo "Welcome to empwage code"
+#!/bin/bash -x
+#Adding part time Employee wage
+#Take constant for code
+isparttime=1;
+isfulltime=2;
+empRatePerHr=20;
+
+#Generating input using random command
+	randomCheck=$((RANDOM%3))
+#Using if condition to generate salary for parttime and fulltime employee
+
+if [[ $isfulltime -eq $randomCheck ]]
+then
+	empHrs=8
+elif [[ $isparttime -eq $randomCheck ]]
+then
+	empHrs=4
+else
+	empHrs=0
+fi
+salary=$(($empHrs*$empRatePerHr))
+
 
