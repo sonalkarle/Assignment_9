0aadfbec6e1500eefe8d37939e09442a3f8a55ad
sonal <sonalkarle01@gmail.com>
2020-12-21 23:07:58 +0530

    Calculating wages for a month using case statment

--- empWage.sh
+++ empWage.sh
@@ -1,2 +1,28 @@
-echo "Welcome to empwage code"
-
+#!/bin/bash -x
+#Calculating the wage for month using case statment
+#Take constant for code
+isfulltime=1
+isparttime=2
+empRatePerHr=20
+numberOfWorkingDays=20
+#Create loop condition for working days
+for (( day=1; day<=$numberOfWorkingDays; day++ ))
+do
+#Genrating input for different condition usign random command
+	empCheck=$(( RANDOM%3 ))
+#Usign case statment used different combination
+case $empCheck in
+	$isfulltime)
+			emphr=8
+			;;
+	$parttime)
+			emphr=4
+			;;
+		*)
+			emphr=0
+			;;
+	esac
+		salary=$(( $emphr * $empRatePerHr ))
+		
+done
+echo $salary
