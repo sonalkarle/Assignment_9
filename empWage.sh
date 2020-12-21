14bd58bb4dff1d1db255b66201b18d391b216a3c
sonal <sonalkarle01@gmail.com>
2020-12-21 18:47:27 +0530

    Check Employee is Present or Absent

--- empWage.sh
+++ empWage.sh
@@ -1,2 +1,22 @@
-echo "Welcome to empwage code"
+#!/bin/bash -x
+#check employee is present or not 
+#Take one constant
 
+	ispresent=1;
+
+#Generating input with random command
+ 
+	randomCheck=$(( RANDOM%2 ));
+
+#Comparing input with constant using if condition
+
+	if [[ $ispresent -eq  $randomCheck ]]
+
+	then
+
+	echo "Employee is Present"
+
+	else
+
+	echo "Employee is Absent"
+	fi
