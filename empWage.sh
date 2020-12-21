fad55eb3803c5ee3a33585562204601e8ac9e06d
sonal <sonalkarle01@gmail.com>
2020-12-21 19:29:06 +0530

    Employee wage using Case statement

--- empWage.sh
+++ empWage.sh
@@ -1,2 +1,23 @@
-echo "Welcome to empwage code"
+#!/bin/bash -x
+#Solving add parttime and daily wage salary using cases
+#Take constant for code
+isparttime=1
+isfulltime=2
+empRatePerHr=20
+salary=0
+#Generating input using random Command 
+	empCheck=$((RANDOM%3))
+#Using case statement for selection according input
+case $empCheck in
+	$isfulltime)
+			empHrs=8
+			;;
+	$isparttime)
+			empHrs=4
+			;;
+		*)
+			empHrs=0
+			;;
+esac
 
+salary =$(($empHrs*$empRatePerHr))
